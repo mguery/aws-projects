b5b964d776d299b7bdcf63f88b927da486fb3bbf
Marjy Guery <marjy@msguery.com>
2021-05-24 01:24:52 -0400

    Update and rename terraform.tfvars to variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "main_vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
