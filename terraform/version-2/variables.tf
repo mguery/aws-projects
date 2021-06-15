7ec94e760314f8383b96f452971fbaf910f1bbf3
Marjy Guery <mguery@users.noreply.github.com>
2021-06-15 00:21:06 -0400

    Create variables.tf

---
+++ terraform/version-2/variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "main_vpc"
+}
+
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_azs" {
+  description = "AZs for VPC"
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "vpc_intra_subnets" {
+  description = "Intra subnets for VPC"
+  type    = list(string)
+  default = ["10.0.7.0/24", "10.0.8.0/24", "10.0.9.0/24"]
+}
